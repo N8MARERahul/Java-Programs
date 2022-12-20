b0a2283ef3bd51476c258089e72d2c6985077043
N8MARERahul <pal521843@gmail.com>
2022-12-20 18:20:13 +0530

    updated

--- Question_21.java
+++ Question_21.java
@@ -1,10 +1,39 @@
 // Write a program to create your own exception as NegativeSizeException whenever negative values are put in an array.
-public class Question_21 {
-    public static void main(String[] args) {
-        
-        // Tring to initialize an array length with negative value...
-        int[] array = new int[-6];
+import java.util.Arrays;
+import java.util.Scanner;
 
-        System.out.println("Array length: " + array.length);
+class NegativeSizeException extends Exception  {
+    public NegativeSizeException (String Str){
+        super(Str);
+    }
+}
+
+public class Question_21 {
+    static void check (int size) throws NegativeSizeException{
+        if (size < 0)
+            throw new NegativeSizeException("Size of array cannot be nagative");
+        else
+            System.out.println("Array creation succesful");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, i;
+        System.out.print("Enter the size of the array : ");
+        n = sc.nextInt();
+        try {
+            check(n);
+        } catch (NegativeSizeException e) {
+            System.out.println("Exception occured : "+ e);
+            System.exit(0);
+        }
+
+        int arr [] = new int [n];
+        System.out.println("Enter the elements of array : ");
+        for (i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 }
