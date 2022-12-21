52991cd06a648b7f87150481b1f6a91d9f41f9c9
N8MARERahul <pal521843@gmail.com>
2022-12-21 22:19:30 +0530

    updated

--- Question_3.java
+++ Question_3.java
@@ -6,12 +6,24 @@ public class Question_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String:");
-        String string = sc.nextLine();     //Input the string from user...
+        String string = sc.nextLine(); // Input the string from user...
 
-        //Replace all the consonants with ""...
-        string = string.replaceAll("[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]", "");
+        int i, count, k;
+        char ch;
+        String strRes = "", consonant = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+        
+        for (i = 0; i < string.length(); i++) {
+            count = 0;
+            ch = string.charAt(i);
+            for (k = 0; k < consonant.length(); k++) {
+                if (ch == consonant.charAt(k))
+                    count++;
+            }
+            if (count == 0)
+                strRes = strRes + ch;
+        }
 
         System.out.println("After deleting all consonants from the string->>");
-        System.out.println(string);        //Print the string after reamoving all the consonants...
+        System.out.println(strRes); // Print the string after reamoving all the consonants...
     }
 }
